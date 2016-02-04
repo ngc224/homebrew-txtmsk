5d9f2f4cecba5f4a72ecac3e314405bd06729a6a
Yoshihiko Nishida <nishida@md761.local>
2016-02-05 03:37:47 +0900

    version

--- txtmsk.rb
+++ txtmsk.rb
@@ -6,7 +6,7 @@ class Txtmsk < Formula
   homepage 'https://github.com/ngc224/txtmsk'
 
   if OS.mac?
-    url "https://github.com/ngc224/txtmsk/releases/download/v#{HOMEBREW_TXTMSK_VERSION}/txtmsk_darwin_amd64.zip"
+    url "https://github.com/ngc224/txtmsk/releases/download/v#{HOMEBREW_TXTMSK_VERSION}/txtmsk_#{HOMEBREW_TXTMSK_VERSION}_darwin_amd64.zip"
     sha1 '8e68e9421cc986564d3e5fa405cf57d26bec73f6'
   end
 
