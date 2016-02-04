bb688e99086f4a1a23c9e42b00f5cca1a9ef3665
Yoshihiko Nishida <nishida@nishida.local>
2016-02-04 20:19:48 +0900

    0.3.2

--- txtmsk.rb
+++ txtmsk.rb
@@ -1,13 +1,13 @@
 require "formula"
 
-VERSION="0.3.1"
+VERSION="0.3.2"
 
 class Txtmsk < Formula
   homepage 'https://github.com/ngc224/txtmsk'
 
   if OS.mac?
     url "https://github.com/ngc224/txtmsk/releases/download/v#{VERSION}/txtmsk_darwin_amd64.zip"
-    sha1 '9ba4f1cea84ac71a500689ae22ba0aac67b5c47a'
+    sha1 'd20fcf215ea3348817df2a537d00277fa1814b30'
   end
 
   def install
