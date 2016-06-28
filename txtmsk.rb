de054923f412a69720db7de0e2b24b8237650055
Yoshihiko Nishida <nishida@nishida.local>
2016-06-28 13:11:13 +0900

    1.0.4

--- txtmsk.rb
+++ txtmsk.rb
@@ -6,7 +6,7 @@ class Txtmsk < Formula
   homepage 'https://github.com/ngc224/txtmsk'
 
   if OS.mac?
-    url "https://github.com/ngc224/txtmsk/releases/download/v#{HOMEBREW_TXTMSK_VERSION}/txtmsk_#{HOMEBREW_TXTMSK_VERSION}.el_capitan.bottle.tar.gz"
+    url "https://github.com/ngc224/txtmsk/releases/download/v#{HOMEBREW_TXTMSK_VERSION}/txtmsk-#{HOMEBREW_TXTMSK_VERSION}.el_capitan.bottle.tar.gz"
     sha1 'dfaac698b65b85c67e2928141021dc491f4120e9'
   end
 
