f33dd624e28d5d1cfb1be7de20eaa37301691c48
Yoshihiko Nishida <nishida@nishida.local>
2016-06-28 12:38:29 +0900

    1.0.4

--- txtmsk.rb
+++ txtmsk.rb
@@ -1,13 +1,13 @@
 require "formula"
 
-HOMEBREW_TXTMSK_VERSION="1.0.3"
+HOMEBREW_TXTMSK_VERSION="1.0.4"
 
 class Txtmsk < Formula
   homepage 'https://github.com/ngc224/txtmsk'
 
   if OS.mac?
     url "https://github.com/ngc224/txtmsk/releases/download/v#{HOMEBREW_TXTMSK_VERSION}/txtmsk_#{HOMEBREW_TXTMSK_VERSION}_darwin_amd64.zip"
-    sha1 'dd6410549393e8ab297ae48bf6ee2c3f2a3c96df'
+    sha1 'fa2ec8891e664f06b489eaf4a70e2924aa314a34'
   end
 
   def install
