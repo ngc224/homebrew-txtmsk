de438f5a28e807b8ddc784fc5b2e9b4976d8b9d4
Yoshihiko Nishida <nishida@nishida.local>
2016-07-08 13:32:59 +0900

    v1.2.1

--- txtmsk.rb
+++ txtmsk.rb
@@ -1,13 +1,13 @@
 require "formula"
 
-HOMEBREW_TXTMSK_VERSION="1.1.9"
+HOMEBREW_TXTMSK_VERSION="1.2.1"
 
 class Txtmsk < Formula
   homepage "https://github.com/ngc224/txtmsk"
 
   if OS.mac?
     url "https://github.com/ngc224/txtmsk/releases/download/v#{HOMEBREW_TXTMSK_VERSION}/txtmsk-#{HOMEBREW_TXTMSK_VERSION}.el_capitan.bottle.tar.gz"
-    sha256 "21cbfc415f4969685983ec8807d77505ec8c799325fa38ee485d659b6f60a7c6"
+    sha256 "e57125366d7850013f5b8d2309b893896f1b3cca4dc629ddd82c3365bf633b88"
   end
 
   def install
