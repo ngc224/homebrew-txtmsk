594d87fa2bc33e52185eff65e95412173df99c3c
nissy <nishida@ngc224.org>
2017-05-18 18:22:47 +0900

    1.3.1

--- txtmsk.rb
+++ txtmsk.rb
@@ -1,13 +1,13 @@
 require "formula"
 
-HOMEBREW_TXTMSK_VERSION="1.2.2"
+HOMEBREW_TXTMSK_VERSION="1.3.1"
 
 class Txtmsk < Formula
-  homepage "https://github.com/ngc224/txtmsk"
+  homepage "https://github.com/nissy/txtmsk"
 
   if OS.mac?
-    url "https://github.com/ngc224/txtmsk/releases/download/v#{HOMEBREW_TXTMSK_VERSION}/txtmsk-#{HOMEBREW_TXTMSK_VERSION}.el_capitan.bottle.tar.gz"
-    sha256 "63aa888dc9b6c1836784d901370f6b2c331466ae46b0049a71e6a74bf0f2d467"
+    url "https://github.com/nissy/txtmsk/releases/download/v#{HOMEBREW_TXTMSK_VERSION}/txtmsk-#{HOMEBREW_TXTMSK_VERSION}_darwin_amd64.tar.gz"
+    sha256 "d06a2e812fb72faad8b87ff3836af8690ef402acf99c7bdec43c4c54e7d60b1a"
   end
 
   def install
